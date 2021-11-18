add036b098078f202a097a4578ca8002f17b62af
sztoslol <kubuszeski05@wp.pl>
2021-11-18 12:22:55 +0100

    super

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<EditText>(R.id.editTextTextMultiLine).text = supernapis@
+        }
+
+    }
+}
